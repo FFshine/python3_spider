8c30908efc87a4577efd82ecd7806e6e8d9b43bb
FFshine <ffshine9@gmail.com>
2018-05-04 13:58:57 +0800

    add a wallpaper spider of wallpaperup.com

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-## python spider for t66y.com and mmjpg.com
+## python spider for some images
 ### you should install requests and BeautifulSoup4 for python3
 ```bash
 pip install requests
 pip install beautifulsoup4
+```
+
+## spider_meizitu.py
+use for download the hot images of [妹子图](http://www.mmjpg.com/home/)
+
+## spider_t66y*
+old drives will konw what this use for :)
+
+## spider_wallpaperup.py
+it's use for download the "Most download" HQ walpaper form [wallpaperup](https://www.wallpaperup.com/most/downloaded)
+and in china you should use ssr to link this site.
+depandent pysocks pysocks package.
+```bash
+pip install pysocks
 ```
---
+++ spider_wallpaperup.py
@@ -0,0 +1,84 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+
+
+import socket
+import socks
+
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
+socket.socket = socks.socksocket
+
+URL = 'https://www.wallpaperup.com/most/downloaded/'
+BEFOREURL = 'https://www.wallpaperup.com'
+headers = {
+    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
+}
+
+
+def getHTML(url):
+    '''获取html
+    返回html'''
+    try:
+        r = requests.get(url, headers=headers, timeout=10)  
+        r.encoding = 'utf-8'
+        return r.text
+    except:
+        return False
+
+def cookMainPage(html):
+    '''分析主页中html
+    获取图片页html
+    并返回[link]'''
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        thumbLinks = soup.select('.thumb-adv .black a')
+        links = []
+        for i in thumbLinks:
+            links.append(i.get('href'))
+        return links
+
+def cookTheScondePage(html):
+    '''分析图片页html
+    获取下载链接
+    并返回'''
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        imagelinks = soup.select('.card-block a')
+        link = imagelinks[0].get('href')
+        return link
+
+def downloadImage(link, name):
+    '''下载并保存'''
+    imageSrc = link
+    imageName = name
+    print(imageSrc)
+    try:
+        file = requests.get(imageSrc,headers=headers, timeout=100).content
+        if file != "" and file !='bad request!':
+            try:
+                f = open(imageName + '.jpg', 'wb')
+                f.write(file)
+                f.close()
+            except:
+                print("写入出错")
+                return
+        print('下载完成 =>=>' + name)
+    except:
+        print('download 出错！！')
+        pass
+
+def main(index=1):
+    os.chdir('/home/ss/Wallpapers')
+    mainPageHTML = getHTML(URL + str(index))
+    for link in cookMainPage(mainPageHTML):
+        name = re.findall(".*\/(.*)\..*",link)[0]
+        if getHTML(BEFOREURL + link):
+            scondeHTML = getHTML(BEFOREURL + link)
+        imageLink =BEFOREURL + cookTheScondePage(scondeHTML)
+        downloadImage(imageLink, name)
+
+if __name__ == '__main__':
+    index = input('请输入你要爬取的页数：')
+    main(index)
